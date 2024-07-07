b5bd314011b56c37cfa9e8b0595285107b451b64
Umesh7Dixit <umesh7dixit@gmail.com>
2024-07-07 18:56:04 +0530

    portfolio

---
+++ src/Pages/Home/AboutMe.jsx
@@ -0,0 +1,27 @@
+export default function AboutMe() {
+
+
+  return (
+    <section id="AboutMe" className="about--section">
+      <div className="about--section--img">
+        <img src="./img/about-me.png" alt="About Me" />
+      </div>
+      <div className="hero--section--content--box about--section--box">
+        <div className="hero--section--content">
+          <h1 className="skills-section--heading">About Me</h1>
+          <p className="hero--section-description">
+             I’m an enthusiastic B.Tech computer science student with good C, C++, and JavaScript skills. My commitment lies
+          in providing practical knowledge in front-end and back-end development. 
+          </p>
+          <p className="hero--section-description">
+          I’ve worked on developing interactive web
+          applications with Bootstrap, HTML5, CSS, Next.js, Express.js, and MongoDB among other tech stack components
+          </p>
+          <p className="hero--section-description">
+          I am always eager to learn and stay updated with the latest tech trends and tools.
+          </p>
+        </div>
+      </div>
+    </section>
+  );
+}
---
+++ src/Pages/Home/HeroSection.jsx
@@ -0,0 +1,29 @@
+export default function HeroSection() {
+  return (
+    <section id="heroSection" className="hero--section">
+      <div className="hero--section--content--box">
+        <div className="hero--section--content">
+          <p className="section--title">Hey, I'm Umesh Dixit</p>
+          <h1 className="hero--section--title">
+            <span className="hero--section-title--color">Full Stack</span>{" "}
+            <br />
+            Developer
+          </h1>
+          <p className="hero--section-description">
+
+          I’m an enthusiastic B.Tech computer science student with good C, C++, and JavaScript skills. My commitment lies
+          in providing practical knowledge in front-end and back-end development. 
+            <br /> 
+            
+            I’ve worked on developing interactive web
+          applications with Bootstrap, HTML5, CSS, Next.js, Express.js, and MongoDB among other tech stack components
+          </p>
+        </div>
+        <button className="btn btn-primary">Get In Touch</button>
+      </div>
+      <div className="hero--section--img">
+        <img src="./img/hero_img.png" alt="Hero Section" />
+      </div>
+    </section>
+  );
+}
