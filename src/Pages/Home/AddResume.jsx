aa903af086c67c17d20d2d7068ab1864891e96cf
Umesh7Dixit <umesh7dixit@gmail.com>
2024-07-11 15:04:48 +0530

    2

--- src/Pages/Home/AddResume.jsx
+++ src/Pages/Home/AddResume.jsx
@@ -1,16 +1,19 @@
 
 const AddResume = () => {
- 
+
+  const handle = ()=> {
+    window.location.href = "https://umesh7dixit.github.io/UmeshDixit_Portfolio/resume.pdf";
+  }
 
   return (
     <div className="p-4">
-    <a
+    <button
       className="inline-block px-6 py-2 text-white bg-blue-600 rounded hover:bg-blue-700 btn btn-primary"
       style={{ textDecoration: 'none' }}
-      href="/UmeshDixit_Portfolio/resume.pdf"
+      onClick={handle}
     >
       My Resume
-      </a>
+      </button>
     </div>
   );
 };
